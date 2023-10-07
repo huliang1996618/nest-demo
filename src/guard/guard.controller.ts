fda32f3ec4464e5e8411a28c6a729ce2da10c070
earlhu <earlhu@tencent.com>
2023-10-07 10:32:18 +0800

    Swagger的使用

--- src/guard/guard.controller.ts
+++ src/guard/guard.controller.ts
@@ -13,12 +13,16 @@ import { GuardService } from './guard.service';
 import { CreateGuardDto } from './dto/create-guard.dto';
 import { UpdateGuardDto } from './dto/update-guard.dto';
 import { RoleGuard } from './role.guard';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('守卫')
 @Controller('guard')
 @UseGuards(RoleGuard)
 export class GuardController {
   constructor(private readonly guardService: GuardService) {}
 
+  @ApiOperation({ summary: '创建接口', description: 'post接口' })
+  @ApiResponse({ status: 404, description: '找不到' })
   @Post()
   create(@Body() createGuardDto: CreateGuardDto) {
     return this.guardService.create(createGuardDto);
@@ -30,6 +34,7 @@ export class GuardController {
     return this.guardService.findAll();
   }
 
+  @ApiQuery({ name: 'id', required: true, description: '这是个id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.guardService.findOne(+id);
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 import { Response } from './common/response';
 import { Filter } from './common/filter';
@@ -9,16 +10,22 @@ import { RoleGuard } from './guard/role.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // 静态资源的访问
-  app.useStaticAssets(join(__dirname, 'images'));
-  // 使用拦截器
-  app.useGlobalInterceptors(new Response());
-  // 使用异常拦截器
-  app.useGlobalFilters(new Filter());
-  // 使用Pipe
-  app.useGlobalPipes(new ValidationPipe());
-  // 使用Guard
-  // app.useGlobalGuards(new RoleGuard());
+  // // 静态资源的访问
+  // app.useStaticAssets(join(__dirname, 'images'));
+  // // 使用拦截器
+  // app.useGlobalInterceptors(new Response());
+  // // 使用异常拦截器
+  // app.useGlobalFilters(new Filter());
+  // // 使用Pipe
+  // app.useGlobalPipes(new ValidationPipe());
+  // // 使用Guard
+  // // app.useGlobalGuards(new RoleGuard());
+  const options = new DocumentBuilder()
+    .setTitle('Nest接口文档')
+    .setDescription('接口文档Demo')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api-doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
